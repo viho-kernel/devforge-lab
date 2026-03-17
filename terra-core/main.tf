dda48940898e6a989b8947c1a56274d43ad6834d
viho-kernel <vihari.reddy1802@gmail.com>
2026-03-18 02:08:01 +0530

    latest commit

--- terra-core/main.tf
+++ terra-core/main.tf
@@ -25,8 +25,60 @@ module "developer_readonly_role" {
       Resource = "*"
     }
   ]
+}
+
+module "ec2_instance_role" {
+  source = "./iam-role"
+
+  project          = var.project
+  environment      = var.environment
+  role_name        = "ec2-instance"
+  owner            = var.owner
+  role_description = "Role attached to EC2 instances for AWS service access"
+
+  trusted_arns = [
+    "arn:aws:iam::992989046853:root"
+  ]
 
   create_instance_profile = true
+
+  policy_statements = [
+    {
+      Sid    = "AllowS3ReadAccess"
+      Effect = "Allow"
+      Action = [
+        "s3:GetObject",
+        "s3:ListBucket"
+      ]
+      Resource = [
+        "arn:aws:s3:::devforge-lab-dev-backend-bucket",
+        "arn:aws:s3:::devforge-lab-dev-backend-bucket/*"
+      ]
+    },
+    {
+      Sid    = "AllowSSMAccess"
+      Effect = "Allow"
+      Action = [
+        "ssm:GetParameter",
+        "ssm:GetParameters",
+        "ssm:GetParametersByPath",
+        "ssmmessages:*",
+        "ssm:UpdateInstanceInformation"
+      ]
+      Resource = "*"
+    },
+    {
+      Sid    = "AllowCloudWatchLogs"
+      Effect = "Allow"
+      Action = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogStreams"
+      ]
+      Resource = "*"
+    }
+  ]
 
 
 }
